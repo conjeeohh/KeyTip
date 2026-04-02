ed32dbb3f8b1e3c223a90701be4c897f5d03038e
conjee <conjeeohh@163.com>
2026-04-02 10:34:08 +0800

    Initial Commit

---
+++ KeyTip/KeyTipApp.swift
@@ -0,0 +1,32 @@
+//
+//  KeyTipApp.swift
+//  KeyTip
+//
+//  Created by 粥太浓了 on 2026/4/2.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KeyTipApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ KeyTipTests/KeyTipTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyTipTests.swift
+//  KeyTipTests
+//
+//  Created by 粥太浓了 on 2026/4/2.
+//
+
+import Testing
+@testable import KeyTip
+
+struct KeyTipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
